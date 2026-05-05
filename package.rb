52511a4275d5ea7a5937da1758de501a1aed25a3
Jason Kraus <jason@focalheat.co>
2026-05-04 21:55:43 -0700

    ruby package: narrow plugin inclusion

--- package.rb
+++ package.rb
@@ -15,8 +15,10 @@ output = File.join(DIST_DIR, "sketchup-link-#{version}.rbz")
 FileUtils.mkdir_p(DIST_DIR)
 FileUtils.rm_f(output)
 
-entries = Dir.glob(File.join(EXT_DIR, '**', '*')).reject { |f| File.directory?(f) || f.start_with?(File.join(EXT_DIR, 'integration/')) }
-
+entries = []
+ext_root = File.join(EXT_DIR, 'sketchup_link.rb')
+entries << ext_root if File.file?(ext_root)
+entries += Dir.glob(File.join(EXT_DIR, 'sketchup_link', '**', '*.rb')).reject { |f| File.directory?(f) }
 Zip::File.open(output, create: true) do |zip|
   entries.each do |abs|
     rel = abs.sub("#{EXT_DIR}/", '')
